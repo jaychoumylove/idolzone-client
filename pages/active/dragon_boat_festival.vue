1d9841f1d2e477a2d9babe69d32cecb7d7412209
zlf <2878253979@qq.com>
2020-06-18 21:48:40 +0800

    端午活动样式修改

--- pages/active/dragon_boat_festival.vue
+++ pages/active/dragon_boat_festival.vue
@@ -66,10 +66,100 @@
 				</view>
 			</view>
 		</view>
+		<!-- 退出活动 -->
 		<prompt v-if="modal=='exit'" title="退出活动粉丝团人气将清零,重新参加则从零开始" placeholder="输入你的ID确认退出本次活动" @confirm="exitGroup"
 		 @closeModal="modal=''">
 		
 		</prompt>
+		<!-- 加入活动 -->
+		<modalComponent v-if="modal == 'joinActivity'" type="center" @closeModal="modal=''">
+			<view class="joinActivity flex-set">
+				<view class="box">
+					<view class="title">
+						<view>参加粉丝团百人人气PK</view>
+						<view>黄金场</view>
+						<view>一个粉丝团只能参加一个场次PK</view>
+						<view>可随时退出，退出人气全部清零</view>
+					</view>
+					<view class="btn-confirm">
+						<view>
+							<btnComponent type="default">
+								<button class="btn">
+									<view class="flex-set" style="width: 200rpx; height: 60rpx;">取消</view>
+								</button>
+							</btnComponent>
+						</view>
+						<view>
+							<btnComponent type="default">
+								<button class="btn">
+									<view class="flex-set" style="width: 200rpx; height: 60rpx;">确认</view>
+								</button>
+							</btnComponent>
+						</view>
+					</view>
+		
+				</view>
+		
+			</view>
+		</modalComponent>
+		<!-- 提示团长管理员才可参加活动 -->
+		<modalComponent v-if="modal == 'tipsJoinActivity'" type="center" @closeModal="modal=''">
+			<view class="joinActivity flex-set">
+				<view class="box">
+					<view class="title">
+						<view style="color: #f00f00;">提示</view>
+						<view>只有粉丝团团长和管理员才能报名参加粉丝团前一百名成员人气PK</view>
+					</view>
+					<view class="btn-confirm">
+						<view>
+							<btnComponent type="default">
+								<button class="btn">
+									<view class="flex-set" style="width: 200rpx; height: 60rpx;">我知道了</view>
+								</button>
+							</btnComponent>
+						</view>
+						<view>
+							<btnComponent type="default">
+								<button class="btn">
+									<view class="flex-set" style="width: 200rpx; height: 60rpx;">喊团长报名</view>
+								</button>
+							</btnComponent>
+						</view>
+					</view>
+		
+				</view>
+		
+			</view>
+		</modalComponent>
+		<!-- 提示加入粉丝团才可参加活动 -->
+		<modalComponent v-if="modal == 'tipsJoinFunsclub'" type="center" @closeModal="modal=''">
+			<view class="joinActivity flex-set">
+				<view class="box">
+					<view class="title">
+						<view style="color: #f00f00;">提示</view>
+						<view>你还没有加入粉丝团，无法参加本次活动，快前往加入吧！</view>
+					</view>
+					<view class="btn-confirm">
+						<view>
+							<btnComponent type="default">
+								<button class="btn">
+									<view class="flex-set" style="width: 200rpx; height: 60rpx;">我知道了</view>
+								</button>
+							</btnComponent>
+						</view>
+						<view>
+							<btnComponent type="default">
+								<button class="btn">
+									<view class="flex-set" style="width: 200rpx; height: 60rpx;">去加入粉丝团</view>
+								</button>
+							</btnComponent>
+						</view>
+					</view>
+		
+				</view>
+		
+			</view>
+		</modalComponent>
 	</view>
 </template>
 
@@ -86,7 +176,7 @@
 		data() {
 			return {
 				funsclubRank: [],
-				modal:'exit',
+				modal:'',
 			};
 		},
 		onLoad(option) {
@@ -208,6 +298,33 @@
 
 
 			}
+		}
+		
+		.joinActivity{
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			
+			.box{
+				width: 80%;
+				
+				.title{
+					width: 100%;
+					font-weight: bold;
+					view{
+						display: flex;
+						align-items: center;
+						justify-content: center;
+					}
+				}
+				.btn-confirm{
+					width: 100%;
+					display: flex;
+					justify-content: space-around;
+					margin: 20rpx 0;
+				}
+			}
+			
 		}
 
 	}
