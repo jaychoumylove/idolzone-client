ac3be3ab99f465d6e54b48a28fc524aac46b16ac
caixian <qq52577517@163.com>
2020-07-16 17:25:39 +0800

    暂时保存

---
+++ pages/active_conform/active_conform.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="container">
+		<view class="banner">
+			
+		</view>
+		<scroll-view class="">
+			
+		</scroll-view>
+		<view class=""></view>
+		<view class=""></view>
+	</view>
+</template>
+
+<script>
+	import bannerComponent from "@/components/bannerComponent.vue";
+	
+	export default {
+		components:{
+			bannerComponent
+		},
+		
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped style="sass">
+	.container {
+		
+	}
+</style>
